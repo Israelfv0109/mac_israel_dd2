27cab188b6145e7bb7017d2418dc1a94bb191499
Israel <israelfv1280@gmail.com>
2026-01-28 00:06:58 +0000

    ajustes en clase makefile y gitignore

--- verif/xcelium.d/run.lnx8664.25.03.d/files.ts
+++ verif/xcelium.d/run.lnx8664.25.03.d/files.ts
@@ -1,2 +1,4 @@
-1769117423 /home/israel/IsraelDD2026/rtl/booth_rtl.sv
-1769117423 /home/israel/IsraelDD2026/verif/booth_tb.sv
+1769557454 /home/israel/IsraelDD2026/rtl/lvl3_rtl.sv
+1769557454 /home/israel/IsraelDD2026/rtl/lvl2_rtl.sv
+1769557454 /home/israel/IsraelDD2026/rtl/lvl1_rtl.sv
+1769553057 /home/israel/IsraelDD2026/verif/mac_tb.sv
