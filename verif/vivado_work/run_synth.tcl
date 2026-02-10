f7794429c584062f1872c654fcd7750a585d2f5e
Israel <israelfv1280@gmail.com>
2026-02-10 16:30:45 -0600

    mod gitignore

--- verif/vivado_work/run_synth.tcl
+++ verif/vivado_work/run_synth.tcl
@@ -1,13 +1,13 @@
-# 1. Leer los archivos RTL
-read_verilog -sv [glob rtl/*.sv]
+# 1. Leer los archivos RTL (Subiendo 2 niveles)
+read_verilog -sv [glob ../../rtl/*.sv]
 
-# 2. Ejecutar Síntesis definiendo el chip (part) aquí mismo
-# Asegúrate que el top se llame mac_top
+# 2. Ejecutar Síntesis
+# Nota: mac_top es el nombre correcto ahora
 synth_design -top mac_top -part xc7a35ticsg324-1L -flatten_hierarchy none
 
 # 3. Generar Reportes
 report_utilization -file utilization_report.txt
 report_timing_summary -file timing_report.txt
 
-# Guardar el diseño sintetizado para abrirlo después sin esperar
+# 4. Guardar Checkpoint
 write_checkpoint -force mac_radix4_synth.dcp
